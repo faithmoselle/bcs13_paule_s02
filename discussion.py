337fe1b241c03fbd0dfe1255ca22d0b917ebde16
Paule <faithmoselle@gmail.com>
2023-03-03 11:01:04 +0800

    Hobbies

--- discussion.py
+++ discussion.py
@@ -3,3 +3,5 @@ print ("Hello Word")
 print ("Name: Faith Paule")
 
 print ("Course: BS Computer Science")
+
+print ("Hobbies: Volleyball, Digital Design, Programming, Movies, and Playing with my pets.")
