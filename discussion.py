761a0267e32dae143f199d0f54b7bdf7e62de346
Paule <faithmoselle@gmail.com>
2023-03-04 11:19:47 +0800

    Add Variables and Data Type in Python

--- discussion.py
+++ discussion.py
@@ -6,3 +6,9 @@ course = "Course: BS Computer Science"
 print (course)
 hobbies = "Hobbies: Volleyball, Digital Design, Programming, Movies, and Playing with my pets."
 print (hobbies)
+
+x = 50 
+print (float(x))
+print (str(x))
+print (bool(x))
+print (int(100))
